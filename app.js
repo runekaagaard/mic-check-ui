6b6a57bfa4ca470647e7d6461d3d2566c57c65dd
jespernasman <jesper.nasman@gmail.com>
2023-07-16 14:32:31 +0100

    Made some changes to the UI, and added danish

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { MediaPermissionsError, MediaPermissionsErrorType, requestAudioPermissions, requestVideoPermissions,
   requestMediaPermissions } from "mic-check"
 import Bowser from "bowser"
-import { en } from "./en"
+import { da } from "./da"
 
 const browser = Bowser.getParser(window.navigator.userAgent)
 
@@ -11,36 +11,63 @@ export const getHelp = function(language, errType) {
   return help
 }
 
-const help = getHelp(en, "PermissionRequired")
-let html = "<h2>Message on page load</h2>"
+const help = getHelp(da, "PermissionRequired")
+let html = document.createElement("div");
+html.innerHTML = "<h2>Message on page load</h2>";
 for (let line of help.help) {
-  html += `<li>${line}</li>`
+  html.innerHTML += `<li>${line}</li>`
 }
-document.getElementById("content").innerHTML += html
-console.log(help)
 
+let modal = document.createElement("div")
+let closeButton = document.createElement("button")
+closeButton.innerHTML = "Close"
+closeButton.onclick = function() {
+  modal.style.display = "none";
+}
+
+modal.style.display = "none"
+modal.style.position = "fixed"
+modal.style.zIndex = "1"
+modal.style.left = "0"
+modal.style.top = "0"
+modal.style.width = "100%"
+modal.style.height = "100%"
+modal.style.overflow = "auto"
+modal.style.backgroundColor = "rgba(0,0,0,0.4)"
+
+let content = document.createElement("div")
+content.style.backgroundColor = "#fefefe"
+content.style.margin = "15% auto"
+content.style.padding = "20px"
+content.style.border = "1px solid #888"
+content.style.width = "80%"
+
+content.appendChild(closeButton)
+content.appendChild(html)
+
+modal.appendChild(content)
+
+document.body.appendChild(modal)
+modal.style.display = "block"
+
+console.log(help)
 
 requestMediaPermissions({audio: true, video: true})
   .then(() => {
     console.log("You got it, buddy!")
+    closeButton.click(); // close the modal when permissions are granted
   })
   .catch((err) => {
-    const help = getHelp(en, err.type)
+    const help = getHelp(da, err.type)
     console.log({help, parsedResult: browser.parsedResult})
-    let html = `
-      <h2>${help.title}</h2>
-      <dl>
-        <dt>Platform</dt>
-        <dd>${browser.parsedResult.platform.type}</dd>
-        <dt>Browser</dt>
-        <dd>${browser.parsedResult.browser.name} ${browser.parsedResult.browser.version}</dd>
-        <dt>Os</dt>
-        <dd>${browser.parsedResult.os.name}</dd>
-      </dl>
-    `
+    let html = document.createElement("div");
+    html.innerHTML = "<br>"
     for (let line of help.help) {
-      html += `<li>${line}</li>`
+      html.innerHTML += `<li>${line}</li>`
     }
-    document.getElementById("content").innerHTML += html
+    content.innerHTML = ''; // Clear previous content
+    content.appendChild(closeButton);
+    content.appendChild(html);
+    modal.style.display = "block" // Display the modal with the error message
   }
 )
---
+++ da.js
@@ -0,0 +1,300 @@
+export const da = {
+  "generic": {
+    "title": "Der opstod et problem",
+    "help": [
+      "Vi stødte på et problem, da vi forsøgte at få adgang til dit kamera og mikrofon.",
+      "Sørg for, at de er korrekt forbundet og fungerer.",
+      "Sørg for, at ingen andre applikationer bruger kameraet eller mikrofonen.",
+      "Kontroller, at din browser har de nødvendige tilladelser til at få adgang til dem.",
+      "Hvis problemet fortsætter, skal du genstarte din browser eller computer."
+    ]
+  },
+  "desktop": {
+    "Chrome": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Browseren har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Åbn indstillingerne på din enhed og gå til Apps & notifikationer sektionen.",
+          "Find Chrome i din liste over apps og gå derefter til App-tilladelser sektionen.",
+          "Slå Kamera og Mikrofon tilladelserne til.",
+          "Genstart til sidst Chrome, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tryk på hængelåsen til venstre for adresselinjen, og slå derefter kameraet og mikrofonen til.",
+          "Genindlæs derefter siden, så ændringerne træder i kraft."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der måske bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi prøvede at få adgang til dit kamera og mikrofon.",
+          "Sørg venligst for, at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet vedvarer, prøv at genstarte din browser eller computer."
+        ]
+      }
+    },
+    "Chromium": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Browseren har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Åbn indstillingerne på din enhed og gå til Apps & notifikationer sektionen.",
+          "Find Chromium i din liste over apps og gå derefter til App-tilladelser sektionen.",
+          "Slå Kamera og Mikrofon tilladelserne til.",
+          "Genstart til sidst Chromium, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tilladelse til at få adgang til kamera og mikrofon blev nægtet.",
+          "Tryk på hængelåsen til venstre for adresselinjen, og slå derefter kameraet og mikrofonen til.",
+          "Genindlæs derefter siden, så ændringerne træder i kraft."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der måske bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi prøvede at få adgang til dit kamera og mikrofon.",
+          "Sørg venligst for, at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet vedvarer, prøv at genstarte din browser eller computer."
+        ]
+      }
+    },
+    "Firefox": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Browseren har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Åbn indstillingerne på din enhed og gå til Apps & notifikationer sektionen.",
+          "Find Firefox i din liste over apps og gå derefter til App-tilladelser sektionen.",
+          "Slå Kamera og Mikrofon tilladelserne til.",
+          "Genstart til sidst Firefox, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tilladelse til at få adgang til kamera og mikrofon blev nægtet.",
+          "Tryk på ikonet med et kamera og en mikrofon til venstre for adresselinjen, og tænd derefter for kameraet og mikrofonen",
+          "Genindlæs derefter siden, så ændringerne træder i kraft."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der måske bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi prøvede at få adgang til dit kamera og mikrofon.",
+          "Sørg venligst for, at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet vedvarer, prøv at genstarte din browser eller computer."
+        ]
+      }
+    },
+    "Microsoft Edge": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Browseren har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Åbn indstillingerne på din enhed og gå til Apps & notifikationer sektionen.",
+          "Find Microsoft Edge i din liste over apps og gå derefter til App-tilladelser sektionen.",
+          "Slå Kamera og Mikrofon tilladelserne til.",
+          "Genstart til sidst Microsoft Edge, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tilladelse til at få adgang til kamera og mikrofon blev nægtet.",
+          "Tryk på hængelåsen til venstre for adresselinjen, og slå derefter kameraet og mikrofonen til.",
+          "Genindlæs derefter siden, så ændringerne træder i kraft."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der måske bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi prøvede at få adgang til dit kamera og mikrofon.",
+          "Sørg venligst for, at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet vedvarer, prøv at genstarte din browser eller computer."
+        ]
+      }
+    },
+    "Safari": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Browseren har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Åbn Systemindstillinger og gå til Sikkerhed & Privatliv.",
+          "Vælg Kamera eller Mikrofon fra listen over muligheder, gå derefter til Privatliv og afkryds boksene ved siden af Safari for at tillade adgang.",
+          "Genstart til sidst Safari, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tilladelse til at få adgang til kamera og mikrofon blev nægtet.",
+          "Genindlæs siden, og vælg \"tillad\".",
+          "Hvis det ikke virker, skal du klikke på \"Safari\" øverst til venstre, vælge \"Indstillinger for [websted]\", og derefter slå kamera og mikrofon til."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der måske bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi prøvede at få adgang til dit kamera og mikrofon.",
+          "Sørg venligst for, at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet vedvarer, prøv at genstarte din browser eller computer."
+        ]
+      }
+    }
+  },
+  "mobile": {
+    "Chrome": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Appen har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Åbn indstillingerne på din enhed og gå til Apps & meddelelser sektionen.",
+          "Find Chrome i din liste over apps og gå derefter til App tilladelser sektionen.",
+          "Slå Kamera og Mikrofon tilladelserne til.",
+          "Genstart til sidst Chrome-appen, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tilladelse til at få adgang til kamera og mikrofon blev nægtet.",
+          "Gå venligst til dine Chrome app webstedsindstillinger og tillad adgang."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der måske bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi forsøgte at få adgang til dit kamera og din mikrofon.",
+          "Sørg for at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet fortsætter, prøv at genstarte din browser eller enhed."
+        ]
+      }
+    },
+    "Safari": {
+      "PermissionRequired": {
+        "title": "Tilladelse krævet",
+        "help": [
+          "Vi skal til at bede om din tilladelse til at få adgang til kameraet og mikrofonen.",
+          "Dette gøres normalt via en pop op-dialogboks."
+        ]
+      },
+      "SystemPermissionDenied": {
+        "title": "Appen har ikke adgang til kamera eller mikrofon",
+        "help": [
+          "Gå til Indstillinger på din enhed og find Safari i listen over applikationer.",
+          "Slå Kamera og Mikrofon tilladelserne til.",
+          "Genstart til sidst Safari-appen, så ændringerne træder i kraft."
+        ]
+      },
+      "UserPermissionDenied": {
+        "title": "Tilladelse nægtet",
+        "help": [
+          "Tilladelse til at få adgang til kamera og mikrofon blev nægtet.",
+          "Gå venligst til dine Safari app webstedsindstillinger og tillad adgang."
+        ]
+      },
+      "CouldNotStartVideoSource": {
+        "title": "En anden applikation bruger kameraet",
+        "help": [
+          "Vi kunne ikke få adgang til kameraet, fordi det bliver brugt af en anden applikation.",
+          "Luk venligst andre applikationer, der muligvis bruger kameraet eller mikrofonen, og genindlæs derefter denne side."
+        ]
+      },
+      "Generic": {
+        "title": "Der opstod en fejl",
+        "help": [
+          "Vi stødte på et problem, mens vi forsøgte at få adgang til dit kamera og din mikrofon.",
+          "Sørg for at de er forbundet og fungerer korrekt, og at ingen andre applikationer eller faner bruger dem.",
+          "Hvis problemet fortsætter, prøv at genstarte din browser eller enhed."
+        ]
+      }
+    }
+  }
+}
+
+
