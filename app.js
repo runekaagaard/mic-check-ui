c44bcb8e62b33ce27e4879061bc26afd6041a6a9
jespernasman <jesper.nasman@gmail.com>
2023-07-25 11:41:37 +0200

    Added platform-specific instructions (windows/macos, ios/android). Android not finished yet.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import { da } from "./da"
 const browser = Bowser.getParser(window.navigator.userAgent)
 
 export const getHelp = function(language, errType) {
-  const help = language[browser.parsedResult.platform.type][browser.parsedResult.browser.name][
+  const help = language[browser.parsedResult.platform.type][browser.parsedResult.os.name][browser.parsedResult.browser.name][
       errType]
   return help
 }
@@ -62,6 +62,17 @@ requestMediaPermissions({audio: true, video: true})
     console.log({help, parsedResult: browser.parsedResult})
     let html = document.createElement("div");
     html.innerHTML = "<br>"
+    html.innerHTML += `
+      <h2>${help.title}</h2>
+      <dl>
+        <dt>Platform</dt>
+        <dd>${browser.parsedResult.platform.type}</dd>
+        <dt>Browser</dt>
+        <dd>${browser.parsedResult.browser.name} ${browser.parsedResult.browser.version}</dd>
+        <dt>Os</dt>
+        <dd>${browser.parsedResult.os.name}</dd>
+      </dl>
+    `
     for (let line of help.help) {
       html.innerHTML += `<li>${line}</li>`
     }
